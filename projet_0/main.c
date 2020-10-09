88c8cf3e2801811af9565c41cf72db155a3faa51
hediturki123 <hedi.turki.sanekli@gmail.com>
2020-10-09 13:03:57 +0200

    qq definitions

--- projet_0/main.c
+++ projet_0/main.c
@@ -6,5 +6,30 @@
 #include <sys/wait.h>
 
 int main() {
+    /*On definit les descripteurs de fichiers du maitre du jeu (processus pere) qui en a 2 : 
+    *    - le premier pour intéragir du maitre au joueur definit par fdMaitre[0]
+    *    - et le deuxieme pour interagir du joueur au maitre definit par fdMaitre[1]
+    * Ensuite on definit un descripteur de fichier par interaction entre 2 joueurs consecutifs
+    */
     
+    int fdMaitre[2][2]; 
+    int fdJoueur[4][2];
+    pipe(fdMaitre[0]);
+    pipe(fdMaitre[1]);
+    pid_t pidMaitre = getpid();
+    int plateau[56];
+    int maisons[6][4];
+    int chevaux[6][4];
+    // Ne pas oublier de considerer que l'on peut jouer de 2 a 4 joueurs
+
+
+
+    for(int i = 0; i < 4; i++) pipe(fdJoueur[i]); // On cree les pipes qui correspondent aux interactions entre joueurs
+    
+    for (int i = 0; i < 6; i++) {
+        if (getpid() == pidMaitre){
+            fork();
+        }
+    }
+
 }
