808a4e0abc1386feab8797d0ebc5ef1023de1203
hediturki123 <hedi.turki.sanekli@gmail.com>
2020-10-21 17:45:13 +0200

    creation struct infoType

--- projet_1/hdr/pc_utils.h
+++ projet_1/hdr/pc_utils.h
@@ -2,4 +2,17 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-int redirect(int oldFd, int newFd);
+#define IT_TOUR_JOUEUR 1
+#define IT_LANCE_DE 2
+
+typedef enum TypeInfo {
+  LANCE_DE = 2,
+  ENDGAME = 0,
+  TOURJOUEUR = 1
+}TypeInfo;
+
+typedef struct Infos {
+  unsigned char validReception;
+  TypeInfo infoType;
+  int info;
+}Infos;
--- projet_1/src/pc_utils.c
+++ projet_1/src/pc_utils.c
@@ -1,7 +1,16 @@
 #include "../hdr/pc_utils.h"
+#include <stdio.h>
 
-int redirect(int oldFd, int newFd) {
-    close(oldFd);
-    dup(newFd);
-    close(newFd);
+
+
+Infos readFile(int fd, int nj) {
+    Infos info;
+    read(fd, &info, sizeof(Infos));
+    printf("Lecture : buf = %d nj = %d\n", info.info, nj);
+    return info;
+}
+
+void writeFile(int fd, int nj, Infos i) {
+    write(fd, &i, sizeof(Infos));
+    printf("Ecriture : buf = %d nj = %d\n", i.info, nj);
 }
