a3db7f7231bb50dd22b12c35edc8614ef6d17997
Hedi Turki <hedi.turki.sanekli@gmail.com>
2020-10-23 22:01:54 +0200

    Update pc_utils.c

--- projet_1/src/pc_utils.c
+++ projet_1/src/pc_utils.c
@@ -1,45 +1,31 @@
 #include "../hdr/pc_utils.h"
-#include <stdio.h>
 
+void interpreteInfo(info_t *info, int numJoueur, int fdJoueur[NOMBRE_JOUEURS][2]) {
+    switch (info->type) {
+        case IT_FIN_PARTIE:
+            write(fdJoueur[numJoueur][1], &info, sizeof(info_t));
+            exit(0);
 
+        case IT_TOUR_JOUEUR:
+            if (info->data == numJoueur) {
+                printf("Joueur %d : c'est mon tour !\n", numJoueur);
+                info->statut = ST_OK;
+            }
+            else if (info->statut == ST_DEMANDE && numJoueur > info->data) {
+                info->statut = ST_ERREUR;
+            }
+            write(fdJoueur[numJoueur][1], &info, sizeof(info_t));
+            break;
 
-Infos readFile(int fd) {
-    Infos info;
-    read(fd, &info, sizeof(Infos));
-    printf("Lecture : buf = %d\n", info.info);
-    return info;
-}
+        case IT_LANCE_DE:
+            if (info->data == numJoueur) {
+                info->statut = ST_OK;
+                info->data = lancerDe6();
+            }
+            write(fdJoueur[numJoueur][1], &info, sizeof(info_t));
+            break;
 
-void writeFile(int fd, Infos i) {
-    write(fd, &i, sizeof(Infos));
-    printf("Ecriture : buf = %d\n", i.info);
-}
-
-
-Infos interpreteInfo(Infos info, int numJoueur){
-  switch(info.infoType){
-    case TypeInfo.ENDGAME :
-      writeFile(fdJoueur[numJoueur][1], info);
-      exit(0);
-      break;
-
-    case TypeInfo.TOURJOUEUR :
-      if (info.info == numJoueur){
-        printf("c'est mon tour, %d\n", numJoueur);
-        info.infoType = TypeInfo.OKTOURJOUEUR;
-      }
-      writeFile(fdJoueur[numJoueur][1], info);
-      break;
-
-    case TypeInfo.LANCE_DE :
-      if (info.info == numJoueur){
-        info.infoType = TypeInfo.OKLANCEDE;
-        info.info = lancerDe6();
-      }
-      writeFile(fdJoueur[numJoueur][1], info);
-      break;
-
-    default :
-      writeFile(fdJoueur[numJoueur][1], info);
-  }
+        default:
+            write(fdJoueur[numJoueur][1], &info, sizeof(info_t));
+        }
 }
